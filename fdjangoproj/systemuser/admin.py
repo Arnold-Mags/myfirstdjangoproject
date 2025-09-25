a2d3f775d3fed0ae36fa3e6045e1a961f898531e
Arnold-Mags <112933384+Arnold-Mags@users.noreply.github.com>
2025-09-25 17:04:23 +0800

    templates

--- fdjangoproj/systemuser/admin.py
+++ fdjangoproj/systemuser/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import SystemUser
 
+admin.site.site_header = "System User Admin"
+admin.site.site_title = "System User Admin Portal"
+admin.site.index_title = "Welcome to System User Portal"
+
+class SystemUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'image_tag', 'contact_number', 'created_at')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('created_at',)
+    ordering = ('username',)
 
 
-admin.site.register(SystemUser)
-
+admin.site.register(SystemUser, SystemUserAdmin)
--- fdjangoproj/systemuser/models.py
+++ fdjangoproj/systemuser/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 from datetime import datetime
 from django.utils import timezone
+import os, random
+from django.utils.html import mark_safe
 
+
+now = timezone.now()
+
+def image_path(instance, filename):
+    basefilename, file_extension= os.path.splitext(filename)
+    chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
+    randomstr = ''.join((random.choice(chars)) for x in range(10))
+    _now = datetime.now()
+
+    return 'profile_pic/{basename}_{randomstring}{ext}'.format(basename=basefilename, randomstring=randomstr, ext=file_extension)
 
 class SystemUser(models.Model):
     first_name = models.CharField(max_length=100, verbose_name="First Name")
@@ -10,7 +22,12 @@ class SystemUser(models.Model):
     contact_number = models.CharField(max_length=15, verbose_name="Contact Number")
     username = models.CharField(max_length=100, verbose_name="Username", unique=True)
     password = models.CharField(max_length=100, verbose_name="Password")
+    user_image = models.ImageField(upload_to=image_path, default='profile_pic/image.png')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
+
+    def image_tag(self):
+        #return mark_safe('<img src="/fdjangoproj/media/%s" width="50" height="50" />' %(self.user_image))
+        return mark_safe(f'<img src="{self.user_image.url}" width="50" height="50" />')
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} {self.email} ({self.username})"

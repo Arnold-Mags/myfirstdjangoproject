185ba88b5039c9924f458da41bd1d56174891486
Arnold-Mags <112933384+Arnold-Mags@users.noreply.github.com>
2025-09-29 19:30:31 +0800

    commit

--- fdjangoproj/systemuser/admin.py
+++ fdjangoproj/systemuser/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import SystemUser
 
 
+class SystemUserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'username', 'image_tag', 'contact_number', 'created_at')
+    search_fields = ('first_name', 'last_name', 'email', 'username')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
 
-admin.site.register(SystemUser)
-
+admin.site.register(SystemUser, SystemUserAdmin)

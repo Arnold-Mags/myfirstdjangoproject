c6075ac979984f2a69d052440514537078279991
Arnold-Mags <112933384+Arnold-Mags@users.noreply.github.com>
2025-10-01 19:31:21 +0800

    working tables

--- fdjangoproj/systemuser/views.py
+++ fdjangoproj/systemuser/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import SystemUser
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'systemuser/index.html')
+    user_list = SystemUser.objects.all()
+    context = {
+        'user_list': user_list
+    }
+    return render(request, 'systemuser/index.html', context)
+

e68c202ed1e2d3e6c39416a56d36ed882b08df88
Arnold-Mags <112933384+Arnold-Mags@users.noreply.github.com>
2025-10-02 19:36:02 +0800

    userlist

--- fdjangoproj/systemuser/views.py
+++ fdjangoproj/systemuser/views.py
@@ -5,9 +5,12 @@ from .models import SystemUser
 # Create your views here.
 
 def index(request):
+    return render(request, 'systemuser/index.html')
+
+def userlist(request):
     user_list = SystemUser.objects.all()
     context = {
         'user_list': user_list
     }
-    return render(request, 'systemuser/index.html', context)
+    return render(request, 'systemuser/userlist.html', context)
 
